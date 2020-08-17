489de4a6ebdcc39e69831adb0eccd14a54657283
David <41311088+biagiola@users.noreply.github.com>
2020-08-17 12:04:41 -0400

    arrow up deleted

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,35 +1,22 @@
-import React, { Component } from 'react';
-import { Link  } from 'react-router-dom';
-import { ThemeContext } from '../contexts/ThemeContext';
-import ThemeToggle from './ThemeToggle';
+import React, { Component } from 'react'
+import { Link  } from 'react-router-dom'
+import { ThemeContext } from '../contexts/ThemeContext'
+import ThemeToggle from './ThemeToggle'
 
 class Home extends Component {
     static contextType = ThemeContext;
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {
             redirect: false
         }
-        this.upFunction = this.upFunction.bind(this);
-        //this.downFunction = this.downFunction.bind(this);
-    }
-
-    upFunction(event){
-        this.setState({
-            redirect: !this.state.redirect
-        })
-    }
-    componentDidMount(){
-        document.addEventListener("keyup", this.upFunction);
-    }
-    componentWillUnmount(){
-        document.removeEventListener("keyup", this.upFunction, false);
     }
 
     render() { 
-        const { isLightTheme, light, dark } = this.context;
-        const theme = isLightTheme ? light : dark;
+        const { isLightTheme, light, dark } = this.context
+        const theme = isLightTheme ? light : dark
         const btnStyle = isLightTheme ? 'btnLight' : 'btnDark'
+
         return (
             <div className="wrapper " style={{ background: theme.bg }}>
                 <ThemeToggle/>
@@ -39,8 +26,8 @@ class Home extends Component {
                     <Link to='/memory'  className={btnStyle}>Memory</Link>
                 </div>
             </div>
-        );
+        )
     }
 }
 
-export default Home;
+export default Home
